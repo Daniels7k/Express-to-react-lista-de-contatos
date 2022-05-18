3474fc82a970d01620c4a3cd6122ae32b7d0e07a
Cleiton Daniel (Kaonami) <danielsdev77@gmail.com>
2022-05-18 06:00:53 -0300

    Mudando dataBase

--- models/mySql.js
+++ models/mySql.js
@@ -1,6 +1,11 @@
 const Sequelize = require("sequelize")
 
-const sequelize = new Sequelize("mysql://b58dce5d4ee02f:e026b76b@us-cdbr-east-05.cleardb.net/heroku_483e41599b4ad9e")
+const heroku_URI = "mysql://b58dce5d4ee02f:e026b76b@us-cdbr-east-05.cleardb.net/heroku_483e41599b4ad9e"
+
+const sequelize = new Sequelize("listadecontatos", "root", "157320My@", {
+  host: "localhost",
+  dialect: "mysql"
+})
 
 try {
     sequelize.authenticate();
